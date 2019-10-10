9a43674b3a5c121910974c43a6492cb48254b9b6
pat-white-code <pat.k.white@gmail.com>
2019-10-10 12:32:55 -0500

    styles tic tac toe board, starts work on reset

--- src/tictactoe/app.js
+++ src/tictactoe/app.js
@@ -11,6 +11,17 @@ const box7 = document.querySelector('.box-7');
 const box8 = document.querySelector('.box-8');
 const box9 = document.querySelector('.box-9');
 
+const playAgain = document.querySelector('.play-again');
+
+function reset() {
+  console.log('reset here')
+  $('.box').css("backgroundColor", "transparent");
+  $('.box').empty();
+  $('.box').removeClass('X');
+  $('.box').removeClass('O');
+  $('.box').setAttribute("onclick", "addGamePiece(this)");
+}
+
 let playerOneScore = 0;
 let playerTwoScore = 0;
 
@@ -35,15 +46,53 @@ function checkForWinner() {
   if(box1.classList.contains(gamePiece) && box2.classList.contains(gamePiece) && box3.classList.contains(gamePiece)) {
     console.log('YOU WIN!')
     finishGame(gamePiece);
+  } else if ((box1.classList.contains(gamePiece) && box4.classList.contains(gamePiece) && box7.classList.contains(gamePiece))) {
+    console.log('YOU WIN!')
+    finishGame(gamePiece);
+  } else if (box1.classList.contains(gamePiece) && box5.classList.contains(gamePiece) && box9.classList.contains(gamePiece)) {
+    console.log('YOU WIN!')
+    finishGame(gamePiece);
+    // BOX 1 WIN SCENARIOS
+  } else if (box2.classList.contains(gamePiece) && box5.classList.contains(gamePiece) && box8.classList.contains(gamePiece)) {
+    console.log('YOU WIN!')
+    finishGame(gamePiece);
+    //BOX 2 WIN SCENARIOS
+  } else if (box1.classList.contains(gamePiece) && box5.classList.contains(gamePiece) && box9.classList.contains(gamePiece)) {
+    console.log('YOU WIN!')
+    finishGame(gamePiece);
+  } else if (box3.classList.contains(gamePiece) && box6.classList.contains(gamePiece) && box9.classList.contains(gamePiece)) {
+    console.log('YOU WIN!')
+    finishGame(gamePiece);
+  } else if (box3.classList.contains(gamePiece) && box5.classList.contains(gamePiece) && box7.classList.contains(gamePiece)) {
+    console.log('YOU WIN!')
+    finishGame(gamePiece);
+    //BOX 3 WIN SCENARIOS
+
+    //BOX 4 WIN SCENARIOS
+  } else if (box4.classList.contains(gamePiece) && box5.classList.contains(gamePiece) && box6.classList.contains(gamePiece)) {
+    console.log('YOU WIN!')
+    finishGame(gamePiece);
+    //BOX 5 WIN SCENARIOS
+  } else if (box7.classList.contains(gamePiece) && box8.classList.contains(gamePiece) && box9.classList.contains(gamePiece)) {
+    console.log('YOU WIN!')
+    finishGame(gamePiece);
   } else {
+    turn++;
+    changePlayer()
     return;
   }
 }
 
 function finishGame(gamePiece) {
   if (gamePiece === 'X') {
+    console.log('Player 1 Wins!')
     document.querySelector('.player-1').style.fontSize="100px";
     playerOneScore++;
+    updateScore();
+  } else {
+    console.log('player 2 Wins!');
+    document.querySelector('.player-2').style.fontSize="100px";
+    playerTwoScore++;
     updateScore();
   }
 }
@@ -84,8 +133,9 @@ function addGamePiece(selectedElement) {
   selectedElement.removeAttribute("onclick");
   selectedElement.className += ' ' + gamePiece;
   checkForWinner();
-  turn++;
-  changePlayer();
+  // turn++;
+  // changePlayer();
+
 }
 //determine player1 or player2
 
