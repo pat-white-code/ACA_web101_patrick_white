b018db7a90a632bf2147209972132f9dcc8678d9
pat-white-code <pat.k.white@gmail.com>
2019-10-11 11:58:42 -0500

    creates prototype for box-generator

--- src/checkpoint-3/app.js
+++ src/checkpoint-3/app.js
@@ -3,10 +3,30 @@ console.log('hello!');
 
 //declare variables
 let clutterBox = $('.clutter-box');
+let gameDisplay = $('.game-display');
 
 //testing JQUERY
 clutterBox.css('backgroundColor', 'yellow');
 
+//create a falling box every 3 seconds
+const interval = setInterval(function() {
+  let newBox = "<div class='clutter-box'></div>";
+  gameDisplay.append(newBox);
+
+}, 3000);
+clearInterval(interval);
+
+//clutter-box-generator:
+
+gameDisplay.on('click', ()=>{
+
+  //deletes previous block:
+  gameDisplay.empty();
+  
+  //creates new clutter-box:
+  let newBox = "<div class='clutter-box'></div>";
+  gameDisplay.append(newBox);
+})
 
 clutterBox.on('click', ()=> {
   clutterBox.remove();
