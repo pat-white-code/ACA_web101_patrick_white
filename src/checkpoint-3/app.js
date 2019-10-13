c17da06d5710bd1c0ce068c3a8d9c76a6a02211f
pat-white-code <pat.k.white@gmail.com>
2019-10-13 14:55:49 -0500

    formats and comments code

--- src/checkpoint-3/app.js
+++ src/checkpoint-3/app.js
@@ -1,6 +1,17 @@
 //make sure js is connected...
 console.log('hello!');
 
+//declare variables
+let clutterBox = $('.clutter-box');
+let gameDisplay = $('.game-display');
+let healthDisplay = $('#health-display');
+let scoreDisplay = $('#score-display');
+let moneyDisplay = $('#money-display');
+let yardsale = $('#yardsale');
+let yardsaleAllowed = $('#yardsale-allower');
+
+
+//Arrays of classes for use with pickRandom() and makeClutter(). styled in styles.css
 const positionArray = ['position-1', 'position-2', 'position-3', 'position-4', 'position-5', 'position-6', 'position-7', 'position-8', 'position-9', 'position-10', 'position-11', 'position-12', 'position-13'];
 
 const colorsArray = ['coral', 'dark-olive-green', 'dim-grey'];
@@ -12,66 +23,78 @@ function pickRandom(array) {
   return array[randomIndex];
 }
 
-//declare variables
-let clutterBox = $('.clutter-box');
-let gameDisplay = $('.game-display');
-let healthDisplay = $('#health-display');
-let scoreDisplay = $('#score-display');
-let moneyDisplay = $('#money-display');
-let yardsale = $('#yardsale');
-let yardsaleAllowed = $('#yardsale-allower');
-
-//Set game starting stats
-
-// console.log('user score: ', userScore)
-
-// console.log(userScore.html());
-
-//
-
+//Set game starting stats and displays
 //User starts out with 5 Health
 let userHealth = 5
+
+//User starts out with 0 points
+let userScore = 0;
 
 //User starts out with $0
 let userMoney = 0;
 
-//Initializes money display
-moneyDisplay.html('$' + userMoney);
+//function for updating health display
+function updateHealthDisplay(){
+  healthDisplay.html(userHealth);
+}
 
-//sets userHealth in display
-healthDisplay.html(userHealth);
+//function for updating money display:
+function updateMoneyDisplay(){
+  moneyDisplay.html('$' + userMoney);
+}
+
+//update score display 
+function updateScoreDisplay() {
+  scoreDisplay.html(userScore);
+}
+
+//Initializes Score Display
+updateScoreDisplay();
+
+//Initializes money display
+updateMoneyDisplay();
+
+//Initializes user health display
+updateHealthDisplay();
+
 
 //function for hosting yardsale
 function hostYardsale() {
-  if(userScore >= 1 ) {
+  //Checks if user has the points
+  if(userScore >= 5 ) {
+    //for testing purposes...
   console.log('hosting yard sale');
-  userScore = userScore - 1;
+
+  //Yard sale loses 1 clutter
+  userScore = userScore - 5;
+
+  //To update score:
   updateScoreDisplay();
-  userMoney = userMoney + 1;
+
+  //Player gains 1 money
+  userMoney = userMoney + 10;
+
+  //Updates money display:
   updateMoneyDisplay();
+
+  //See if user still has money for the next yardsale?
   checkIfYardsale();
   } else {
+    //If player does not have money for yard sale, do nothing.
     return;
   } 
-  // checkIfYardsale();
 }
 
 //function for checking if yardsale is possible.
 function checkIfYardsale() {
-  console.log('checking if yard sale...')
   //if user has more than 5 points, allow yard sale
-  if(userScore >= 1) {
-    console.log('yard sale is allowed..')
+  if(userScore >= 5) {
+    //Style button to be clickable
     yardsale.addClass('active')
   } else {
-    console.log('yardsale not allowed...')
     // remove properties previously set by allow yard sale
     yardsale.removeClass('active');
   }
-}
-
-function allowYardsale() {
-  yardsale.addClass('active');
 }
 
 //function for user loses one health
@@ -81,29 +104,8 @@ function loseHealth(){
   updateHealthDisplay();
 }
 
-//function for updating health display
-function updateHealthDisplay(){
-  healthDisplay.html(userHealth);
-}
-
-//function for updating money display:
-function updateMoneyDisplay(){
-  moneyDisplay.html(userMoney);
-}
-
-//update score display 
-function updateScoreDisplay() {
-  scoreDisplay.html(userScore);
-}
-
-//User starts out with 0 points
-let userScore = 0;
-
-//User starts game with 0 points
-scoreDisplay.html(userScore);
-
 //Function for updating score 
-function updateScore() {
+function addPoint() {
   userScore++;
   console.log(userScore);
   updateScoreDisplay();
@@ -113,9 +115,7 @@ function updateScore() {
 //CREATES TIMED CLUTTER-DROPS
 function clutterMaker()
 {
-  x = 5;  // Sets repeated timer for clutter-maker in Seconds
-
-  // gameDisplay.empty(); // Clears game display if needed.
+  x = 2;  // Sets repeated timer for clutter-maker in Seconds
 
   //Sets random color/clutter-type
   let randomColor = pickRandom(colorsArray);
@@ -133,15 +133,15 @@ function clutterMaker()
 
   //When user clicks on clutter, destroy clutter
   newBox.on('click', ()=> {
-    newBox.removeAttr('animationend');
+    newBox.off('animationend');
     newBox.remove();
-    updateScore();
+    addPoint();
   })
-
   //Drops the new clutter-box
-// FIXME: 'animationend' event happens when animation starts. why???
   gameDisplay.append(newBox);
   newBox.on('animationend', ()=>{
+    //If the box finishes animation, lose 1 health and remove the box
+    loseHealth();
     newBox.remove();
   })
 
